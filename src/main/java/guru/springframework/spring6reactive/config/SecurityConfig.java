045350276ebbc3464e6e51147275834e514edc5f
ivanxalie <ivanxalie@gmail.com>
2024-06-05 19:45:47 +0300

    Fix (or add, I don't remember) security.

--- src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6reactive/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring6reactive.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -13,9 +15,11 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(spec -> spec.matchers(EndpointRequest.toAnyEndpoint()).permitAll())
                 .authorizeExchange(spec -> spec.anyExchange().authenticated())
                 .oauth2ResourceServer(spec -> spec.jwt(withDefaults()))
                 .build();
